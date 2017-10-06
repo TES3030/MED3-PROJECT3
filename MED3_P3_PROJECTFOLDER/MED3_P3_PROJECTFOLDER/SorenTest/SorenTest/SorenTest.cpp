4d399709b60be2cd0ac2f530974eb79313f9e819
Søren Skouv <soerenskouv@gmail.com>
2017-10-06 10:47:36 +0200

    This was just a test from Søren

---
+++ MED3_P3_PROJECTFOLDER/MED3_P3_PROJECTFOLDER/SorenTest/SorenTest/SorenTest.cpp
@@ -0,0 +1,16 @@
+// SorenTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Who's your daddy?" << endl;
+	cout << "You are ;)" << endl;
+
+    return 0;
+}
+
