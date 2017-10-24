e893ab31011fcb6b0e5f38e4ef92b990eec82e47
JuhaVuorenalho <jvuore16@student.aau.dk>
2017-10-24 13:59:48 +0200

    Detecting Red Color from Webcam

--- SemesterProject_0.1/SemesterProject_0.1/SemesterProject01.cpp
+++ SemesterProject_0.1/SemesterProject_0.1/SemesterProject01.cpp
@@ -5,8 +5,56 @@
 //COMMIT BY JUHA 3rd try
 //COMMIT BY TOBI
 
-int main()
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
 {
-    return 0;
+	VideoCapture cap(0); //Capture the video from web cam
+
+	if (!cap.isOpened())  //If cannot open wwebcam, exit
+	{
+		cout << "Cannot open the web cam" << endl;
+		return -1;
+	}
+
+
+	while (true)
+	{
+		Mat imgOriginal;
+		Mat imgBlurred;
+
+		bool bSuccess = cap.read(imgOriginal); // Capture frame from the video
+
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+
+		Mat imgRedOutput;
+
+		inRange(imgOriginal, Scalar(10, 10, 100), Scalar(30, 30, 255), imgRedOutput); //Thresholding, should be a better way to separate the red
+
+
+		imshow("Original", imgOriginal); //Show the original image
+		imshow("Red", imgRedOutput); //Show the image capturing red
+
+
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
+
+	return 0;
+
 }
 
