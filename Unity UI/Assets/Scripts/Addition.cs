ecffbfb9273b7b530329e74b94b2269dc305925d
TES3030 <5289699+TES3030@users.noreply.github.com>
2017-11-08 16:06:49 +0100

    my changes

---
+++ Unity UI/Assets/Scripts/Addition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.RANDOM;
+
+public class Addition : MonoBehaviour {
+
+
+    public Text txt;
+
+    int[] numbersAvailable = new int[10];
+    Random rnd = new Random();
+    int targetValue;
+
+    // Use this for initialization
+    void Start () {
+        targetValue = rnd.Next(1, 10);
+        txt.text = targetValue.ToString();
+        //generating 3 numbers
+        for (int i; i<numbersAvailable.Length;i++)
+        {
+            rnd.Next();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
